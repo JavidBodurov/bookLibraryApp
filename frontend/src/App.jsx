48819ee0da19079fba67ffe279085e7f4d7f90b3
JavidBodurov <javid.bodurov93@gmail.com>
2025-05-19 13:31:46 -0600

    Add Redux setup and implement book list with addBook feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   return (
     <div className="app">
         <header className='app-header'>
-            <h1>Book Library App0</h1>
+            <h1>Book Library App</h1>
         </header>
         <main className='app-main'>
             <div className='app-left-column'>
                 <BookForm/>
             </div>
-            <div className='app-right-columnn'>
-                {/* <Filter/> */}
+            <div className='app-right-column'>
+                <Filter/>
                 <BookList/>
             </div>
         </main>
